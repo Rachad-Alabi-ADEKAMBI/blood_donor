e027c2e0bf44d724103a34067b6392bd6ef1ba0a
Rachad-Alabi-ADEKAMBI <adekambirachad@gmail.com>
2024-06-10 23:16:01 +0100

    update

---
+++ bloodDonor.php
@@ -0,0 +1,19 @@
+<?php
+/*
+* Plugin Name: Blood donation
+* Description: Wordpress plugin
+* Version: 1.3
+* Requires PHP: 8.0
+* Author: Rachad A. ADEKAMBI
+* Author URI: https://rachad-alabi-adekambi.github.io/portfolio 
+* Text Domain: magic-plugin
+* Domain Path: /languages
+*/
+
+// Ensure the plugin is not directly accessed
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+add_shortcode('magic', 'displayForm');
+
